e43769b3b382009691a1142c8e38dcaca234e9cf
Tyrese <tyresejin3915@gmail.com>
2023-08-03 14:40:40 +0900

    style: font

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "@fontsource/inter";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/400-italic.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
